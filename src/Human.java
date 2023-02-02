8bbb2914c089c189be85f661276ffd5a9c2c95c6
lat-k <kacperlatarski418@gmail.com>
2023-02-02 23:38:50 +0100

    zadanie 3 i 4

--- src/Human.java
+++ src/Human.java
@@ -3,4 +3,31 @@ public class Human {
      int age;
      Animal pet;
      public Car c;
+
+      double salary;
+
+     Double   get_salary()
+     {
+          System.out.println(" Pobrano we wtorek i wynosila:"  + salary);
+          return salary;
+     }
+     public void set_salary( double salary ){
+          if( salary < 0 ){
+               System.out.println("Podano ujemna wartosc wynagrodzenia");
+          }
+          else{
+               this.salary = salary;
+               System.out.println("Wyslano do systemu ksiegowego");
+               System.out.println("Prosze zglosic sie do dzialu kadr");
+               System.out.println("Zglosic sie do ZUS i US");
+          }
+     }
+     }
+
+
+
+
+
+
+
 }
